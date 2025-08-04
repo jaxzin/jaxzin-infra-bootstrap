d5d4b29c3c7ce803f16132f7191ddf01880612e8
Brian R. Jackson <brian@jaxzin.com>
2025-08-04 15:51:52 -0400

    Fix virtual environment activation path in post-create script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -9,6 +9,7 @@ echo "uv version: $(uv --version || echo 'uv not found')"
 # Setup uv with all the Ansible Molecule dependencies
 # Keep the uv virtual environment in the container, rather than the workspace which might be shared with the host
 #  and likely won't have the Python interpreter installed in the same location to symlink to.
+uv venv /home/vscode/.venv
 
 # Create a virtual environment for uv and install the project dependencies
 uv sync
@@ -21,7 +22,7 @@ uv run make deps
 echo 'eval "$(_MOLECULE_COMPLETE=SHELL_source uv run molecule)"' >> /home/vscode/.bashrc
 
 # Drop into the virtual environment
-echo 'source .venv/bin/activate' >> /home/vscode/.bashrc
+echo 'source /home/vscode/.venv/bin/activate' >> /home/vscode/.bashrc
 
 # Ensure the collections path is set correctly for Ansible development
 echo "export ANSIBLE_COLLECTIONS_PATH=\"\$(pwd)/collections:\$(uv run ansible-config dump --format json | jq -r '.[] | select(.name == \"COLLECTIONS_PATHS\") | .value[]' | grep -vxF \$(pwd)/collections | paste -sd: -)\"" >> /home/vscode/.bashrc
